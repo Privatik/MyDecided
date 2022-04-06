e9506ba504b3a7f2e125a0b316166c32912729c5
Android dev <i.kurak@complitech.org>
2022-04-06 12:23:46 +0300

    Decided 45. Jump Game II

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,90 +1,83 @@
-import kotlin.collections.ArrayList
-
+import kotlin.math.max
 
 fun main(args: Array<String>) {
 
-    val head = TreeNode(3).also { root ->
-        root.left = TreeNode(1).also { left ->
-            left.right = TreeNode(2)
-        }
-    }
+//    val head = Solution.TreeNode(3).also { root ->
+//        root.left = TreeNode(1).also { left ->
+//            left.right = TreeNode(2)
+//        }
+//    }
+//
+//    val head1 = TreeNode(1).also { root ->
+//        root.left = TreeNode(2).also { left ->
+//            left.right = TreeNode(3)
+//        }
+//
+//        root.right = TreeNode(2).also { right ->
+//            right.right = TreeNode(3)
+//        }
+//    }
+//
+//    val head2 = TreeNode(2).also { root ->
+//        root.left = TreeNode(3).also { left ->
+//            left.left = TreeNode(4)
+//            left.right = TreeNode(5)
+//        }
+//
+//        root.right = TreeNode(3).also { right ->
+//            right.left = TreeNode(5)
+//        }
+//    }
+//
+//    val link = ListNode(1).apply {
+//        add(mutableListOf(4,3,2,5,2))
+//    }
 
-    val head1 = TreeNode(1).also { root ->
-        root.left = TreeNode(2).also { left ->
-            left.right = TreeNode(3)
-        }
 
-        root.right = TreeNode(2).also { right ->
-            right.right = TreeNode(3)
-        }
-    }
-
-    val head2 = TreeNode(2).also { root ->
-        root.left = TreeNode(3).also { left ->
-            left.left = TreeNode(4)
-            left.right = TreeNode(5)
-        }
-
-        root.right = TreeNode(3).also { right ->
-            right.left = TreeNode(5)
-        }
-    }
-
-    val link = ListNode(1).apply {
-        add(mutableListOf(4,3,2,5,2))
-    }
-
-
-    println(Solution().jump(intArrayOf(2,1)))
+    println(Solution().jump(intArrayOf(2,3,1,1,4)))
 }
 
 
 class Solution {
     fun jump(nums: IntArray): Int {
-        if (nums.size < 2){
-            return 0
-        }
-
-        val array = IntArray(nums.size + 1)
-
-        array[1] = 1
-        array[2] = if (nums[0] > 1) 1 else 2
-        for (index in 2 until nums.size){
-            array[index + 1] = array[index]
-
-            if (nums[index - 2] + (index - 2) > index){
-                array[index + 1]++
+        var jumps = 0
+        var curEnd = 0
+        var curFarthest = 0
+        for (i in nums.indices) {
+            curFarthest = max(curFarthest, i + nums[i])
+            if (i == curEnd) {
+                jumps++
+                curEnd = curFarthest
             }
         }
-
-        return array[nums.size]
+        return jumps
     }
 }
 
 
-class ListNode(var `val`: Int) {
-    var next: ListNode? = null
-
-    fun add(list: MutableList<Int>){
-        val l = list.removeAt(0)
-        next = ListNode(l).also {
-            if (list.isNotEmpty()){
-                it.add(list)
-            }
-        }
-    }
-
-    override fun toString(): String {
-        return "$`val` ${next.toString()}"
-    }
-}
-
-
-class TreeNode(var `val`: Int) {
-    var left: TreeNode? = null
-    var right: TreeNode? = null
-
-    override fun toString(): String {
-        return `val`.toString()
-    }
-}
+//class ListNode(var `val`: Int) {
+//    var next: ListNode? = null
+//
+//    fun add(list: MutableList<Int>){
+//        val l = list.removeAt(0)
+//        next = ListNode(l).also {
+//            if (list.isNotEmpty()){
+//                it.add(list)
+//            }
+//        }
+//    }
+//
+//    override fun toString(): String {
+//        return "$`val` ${next.toString()}"
+//    }
+//}
+//
+//
+//class TreeNode(var `val`: Int) {
+//    var left: TreeNode? = null
+//    var right: TreeNode? = null
+//
+//    override fun toString(): String {
+//        return `val`.toString()
+//    }
+//}
