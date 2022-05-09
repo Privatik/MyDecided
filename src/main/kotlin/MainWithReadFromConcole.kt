905bcebbec0fc20ed9ac10cca4ba59b1c13da1ce
Privatik <kurak.van9@gmail.com>
2022-05-09 13:46:49 +0300

    6 task from ozon

--- src/main/kotlin/MainWithReadFromConcole.kt
+++ src/main/kotlin/MainWithReadFromConcole.kt
@@ -1,41 +1,80 @@
+/*
+3 5
+1 10
+2 9
+3 8
+4 7
+5 6
+ */
+
+data class Session(
+    val n: Int,
+    val workPairs: List<Pair<Int, Int>>
+)
+
 fun main(){
-    readLine()
-    val number = readLine()!!.split(" ").map { it.toInt() }
+    val count = readLine()!!.toInt()
+    val sessions = Array<Session?>(count) { null }
+    val result = StringBuilder()
+    repeat(count){ rootIndex ->
+        readLine()
+        readLine()!!.split(" ").also {
+            val n = it[0].toInt()
+            val k = it[1].toInt()
 
-    var result = Array(number.size + 1){ 0 }
-
-    var counter = -1
-
-    while (counter != result.lastIndex){
-        result = Array(number.size + 1){ 0 }
-        var isNegative = false
-        counter += 1
-        for (index in counter - 1 downTo 0){
-            val element = result[index + 1] - number[index]
-            if (element < 0) {
-                isNegative = true
-                break
+            val list = List(k) {
+                val longValue = readLine()!!.split(" ").map { s -> s.toInt() }
+                Pair(longValue[0], longValue[1])
             }
-            result[index] = element
-        }
-
-        if (isNegative){
-            continue
-        }
-
-        for (index in counter + 1 until result.size){
-            val element = number[index - 1] + result[index - 1]
-            if (element < 0) {
-                isNegative = true
-                break
-            }
-            result[index] = element
-        }
-
-        if (!isNegative){
-            break
+            sessions[rootIndex] = Session(n, list)
         }
     }
 
-    println(result.joinToString(" "))
+    sessions.forEachIndexed { index, session ->
+        val workArr = Array<Int>(session!!.n) { 0 }
+        val resultCurrentWork = StringBuilder()
+
+        session.workPairs.forEach { pair ->
+            var isAdd = false
+
+            var wIndex = 0
+
+            var minThread = 0
+
+            while (wIndex < workArr.size){
+                val currentValue = workArr[wIndex]
+                if (pair.first >= currentValue){
+                    val value = pair.first + pair.second
+                    resultCurrentWork.append(value)
+                    workArr[wIndex] = value
+
+                    isAdd = true
+                    break
+                }
+
+                if (workArr[minThread] > currentValue){
+                    minThread = wIndex
+                }
+
+                wIndex++
+            }
+
+            if (!isAdd){
+                val update = workArr[minThread] + pair.second
+                resultCurrentWork.append(update)
+                workArr[minThread] = update
+            }
+
+            if (index != session.workPairs.size - 1){
+                resultCurrentWork.append(" ")
+            }
+        }
+
+        result.append(resultCurrentWork)
+        if (index != count - 1){
+            result.append("\n")
+        }
+    }
+
+    println(result)
 }
