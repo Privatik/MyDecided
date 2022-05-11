78e5bc07cf078ca56cbe8b94b370274c2af06313
Android dev <i.kurak@complitech.org>
2022-05-11 11:00:02 +0300

    all task from ozon

--- src/main/kotlin/MainWithReadFromConcole.kt
+++ src/main/kotlin/MainWithReadFromConcole.kt
@@ -8,53 +8,97 @@ const val YES = "YES"
 const val NO = "NO"
 
 fun main(){
-    val verticalZero = Array(3) {0}
-    val horizontalZero = Array(3) {0}
+    val count = readLine()!!.toInt()
+    val result = StringBuilder()
+    repeat(count) { rootIndex ->
+        readLine()
+        val verticalZero = Array(3) {0}
+        val horizontalZero = Array(3) {0}
+        var positiveDiagonalZero = 0
+        var negativeDiagonalZero = 0
 
-    val verticalCross = Array(3) {0}
-    val horizontalCross = Array(3) {0}
+        val verticalCross = Array(3) {0}
+        val horizontalCross = Array(3) {0}
+        var positiveDiagonalCross = 0
+        var negativeDiagonalCross = 0
 
-    var countCross = 0
-    var countZero = 0
+        var countCross = 0
+        var countZero = 0
 
-    var isWin = false
+        var isWinCross = false
+        var isWinZero = false
 
-    repeat(3){ row ->
-        readLine()!!.forEachIndexed { column, c ->
-            when (c){
-                CROSS -> {
-                    countCross++
-                    verticalCross[row] += 1
-                    horizontalCross[column] += 1
-                    if (verticalCross[row] == 3 || horizontalCross[column] == 3){
-                        if (isWin){
-                            println(NO)
-                            return
+        repeat(3){ row ->
+            readLine()!!.forEachIndexed { column, c ->
+                when (c){
+                    CROSS -> {
+                        countCross++
+                        verticalCross[row] += 1
+                        horizontalCross[column] += 1
+                        if (column == row) positiveDiagonalCross++
+                        if (row == 2 - column) negativeDiagonalCross++
+                        if (
+                            verticalCross[row] == 3 ||
+                            horizontalCross[column] == 3 ||
+                            positiveDiagonalCross == 3 ||
+                            negativeDiagonalCross == 3
+                        ){
+                            isWinCross = true
                         }
-                        isWin = true
                     }
-                }
-                ZERO -> {
-                    countZero++
-                    verticalZero[row] += 1
-                    horizontalZero[column] += 1
-                    if (verticalZero[row] == 3 || horizontalZero[column] == 3){
-                        if (isWin){
-                            println(NO)
-                            return
+                    ZERO -> {
+                        countZero++
+                        verticalZero[row] += 1
+                        horizontalZero[column] += 1
+                        if (column == row) positiveDiagonalZero++
+                        if (row == 2 - column) negativeDiagonalZero++
+                        if (verticalZero[row] == 3 ||
+                            horizontalZero[column] == 3 ||
+                            positiveDiagonalZero == 3 ||
+                            negativeDiagonalZero == 3
+                        ){
+                            isWinZero = true
                         }
-                        isWin = true
                     }
+                    EMPTY -> { }
                 }
-                EMPTY -> { }
             }
         }
+
+        if (isWinCross && isWinZero){
+            result.append(NO)
+            result.append("\n")
+            return@repeat
+        }
+
+        if (isWinCross){
+            if (countCross - countZero != 1){
+                result.append(NO)
+                result.append("\n")
+                return@repeat
+            }
+        }
+
+        if (isWinZero){
+            if (countCross - countZero != 0){
+                result.append(NO)
+                result.append("\n")
+                return@repeat
+            }
+        }
+
+        if (countCross - countZero !in 0..1){
+            result.append(NO)
+            result.append("\n")
+            return@repeat
+        }
+
+        result.append(YES)
+        if (rootIndex != count - 1){
+            result.append("\n")
+        }
+
     }
 
-    if (abs(countCross - countZero) > 1){
-        println(NO)
-        return
-    }
-
-    println(YES)
+    println(result)
 }
