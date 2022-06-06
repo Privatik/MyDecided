84c78fddff06479ccaf3279d96e36804565e4497
Android dev <i.kurak@complitech.org>
2022-06-06 15:50:38 +0300

    Solved 160. Intersection of Two Linked Lists

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,3 @@
-import kotlin.math.abs
-
 fun main(args: Array<String>) {
 
 //    val head = Solution.TreeNode(3).also { root ->
@@ -45,59 +43,59 @@ fun main(args: Array<String>) {
     val strArr = arrayOf("test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com")
     val strArr2 = arrayOf("a@leetcode.com","b@leetcode.com","c@leetcode.com")
 
-    val array = arrayOf(
-        intArrayOf(3, 0, 1, 4, 2),
-        intArrayOf(5, 6, 3, 2, 1),
-        intArrayOf(1, 2, 0, 1, 5),
-        intArrayOf(4, 1, 0, 1, 7),
-        intArrayOf(1, 0, 3, 0, 5)
-    )
-
-    array.forEach {
-        println(it.joinToString(" "))
+    val headA1 = ListNode(4).apply {
+        add(mutableListOf(1,8,4,5))
     }
-    println("-------")
-    val numMatrix = NumMatrix(array)
-    println(numMatrix.sumRegion(2,1,4,3))
-    println(numMatrix.sumRegion(1,1,2,2))
-    println(numMatrix.sumRegion(1,2,2,4))
+    val headB1 = ListNode(5).apply {
+        add(mutableListOf(6,1,8,4,5))
+    }
+
+    val headA2 = ListNode(1).apply {
+        add(mutableListOf(9,1,2,4))
+    }
+    val headB2 = ListNode(3).apply {
+        add(mutableListOf(2,4))
+    }
+
+    println(Solution().getIntersectionNode(headA1,headB1))
+    println(Solution().getIntersectionNode(headA2,headB2))
 }
 
 
 class Solution {
+    fun getIntersectionNode(headA:ListNode?, headB:ListNode?):ListNode? {
+        var p1 = headA
+        var p2 = headB
 
-}
+        var len1 = 0
+        var len2 = 0
 
-class NumMatrix(matrix: Array<IntArray>) {
+        while (p1 != null) {
+            p1 = p1.next
+            len1++
+        }
+        while (p2 != null) {
+            p2 = p2.next
+            len2++
+        }
 
-    val acc = Array<IntArray>(matrix.size) { IntArray(matrix[0].size) }
-
-    init {
-        for (i in matrix.indices) {
-            var rowSum = 0
-            for (j in 0 until matrix[0].size) {
-                rowSum += matrix.at(i, j)
-                acc[i][j] = rowSum + acc.at(i - 1, j)
+        p1 = headA
+        p2 = headB
+        if (len1 > len2) {
+            for (i in 0 until len1 - len2) {
+                p1 = p1!!.next
+            }
+        } else {
+            for (i in 0 until len2 - len1) {
+                p2 = p2!!.next
             }
         }
-
-        println("------")
-        acc.forEach {
-            println(it.joinToString(" "))
+        while (p1 != p2) {
+            p1 = p1!!.next
+            p2 = p2!!.next
         }
-        println("------")
+        return p1
     }
-
-    fun sumRegion(row1: Int, col1: Int, row2: Int, col2: Int): Int {
-        return acc[row2][col2] - acc.at(row1 - 1, col2) - acc.at(row2, col1 - 1) + acc.at(row1 - 1, col1 - 1)
-    }
-
-    fun Array<IntArray>.at(i: Int, j: Int): Int {
-        if(i < 0 || i >= size || j < 0 || j >= this[0].size) return 0
-
-        return this[i][j]
-    }
-
 }
 
 
