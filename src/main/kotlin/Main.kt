c903248029e3fd33d83b268c290e5193af94456c
Android dev <i.kurak@complitech.org>
2022-06-08 14:55:19 +0300

    Solved 88. Merge Sorted Array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,3 @@
-import kotlin.math.min
-
 fun main(args: Array<String>) {
 
 //    val head = Solution.TreeNode(3).also { root ->
@@ -49,59 +47,35 @@ fun main(args: Array<String>) {
     val num2 = intArrayOf(2,5,6)
     Solution().merge(num1, 3, num2, 3)
     println(num1.joinToString(" "))
+//
+//    val num3 = intArrayOf(1)
+//    val num4 = intArrayOf()
+//    Solution().merge(num3, 1, num4, 0)
+//    println(num3.joinToString(" "))
+//
+//    val num5 = intArrayOf(0)
+//    val num6 = intArrayOf(1)
+//    Solution().merge(num5, 0, num6, 1)
+//    println(num5.joinToString(" "))
 
-    val num3 = intArrayOf(1)
-    val num4 = intArrayOf()
-    Solution().merge(num3, 1, num4, 0)
-    println(num3.joinToString(" "))
-
-    val num5 = intArrayOf(0)
-    val num6 = intArrayOf(1)
-    Solution().merge(num5, 0, num6, 1)
-    println(num5.joinToString(" "))
-
-    val num7 = intArrayOf(2,0)
-    val num8 = intArrayOf(1)
-    Solution().merge(num7, 1, num8, 1)
-    println(num7.joinToString(" "))
+//    val num7 = intArrayOf(2,0)
+//    val num8 = intArrayOf(1)
+//    Solution().merge(num7, 1, num8, 1)
+//    println(num7.joinToString(" "))
 }
 
 
 class Solution {
     fun merge(nums1: IntArray, m: Int, nums2: IntArray, n: Int) {
-        if (n == 0) return
-        if (m == 0){
-            for (i in nums1.indices){
-                nums1[i] = nums2[i]
-            }
+        var tail1 = m - 1
+        var tail2 = n - 1
+        var finished = m + n - 1
+        while (tail1 >= 0 && tail2 >= 0) {
+            nums1[finished--] = if (nums1[tail1] > nums2[tail2]) nums1[tail1--] else nums2[tail2--]
         }
 
-        var point1 = 0
-        var point2 = 0
-
-        val answer = IntArray(nums1.size)
-
-        while (answer.size > point1 + point2){
-            val index = point1 + point2
-
-            val isMore = nums1[point1] > nums2[point2]
-            val min = if (isMore){
-                nums2[point2]
-            } else {
-                nums1[point1]
-            }
-
-            answer[index] = min
-
-            if (isMore){
-                point2 = if (point2 + 1 < n - 1) point2 + 1 else n - 1
-            } else {
-                point1 = if (point1 + 1 < m - 1) point1 + 1 else m - 1
-            }
-        }
-
-        for (i in nums1.indices){
-            nums1[i] = answer[i]
+        while (tail2 >= 0) {
+            nums1[finished--] = nums2[tail2--]
         }
     }
 }
