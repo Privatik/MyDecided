7f63258b5b45db455035c69e490646608cfa91c4
Android dev <i.kurak@complitech.org>
2023-03-24 09:42:10 +0300

    1466. Reorder Routes to Make All Paths Lead to the City Zero

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,74 @@
+import java.util.*
+
 fun main(args: Array<String>) {
     println(
-        minScore(
-            4,
+        minReorder(
+            6,
             arrayOf(
-                intArrayOf(1,2,9),
-                intArrayOf(2,3,6),
-                intArrayOf(2,4,5),
-                intArrayOf(1,4,7),
+                intArrayOf(0,1),
+                intArrayOf(1,3),
+                intArrayOf(2,3),
+                intArrayOf(4,0),
+                intArrayOf(4,5),
             )
         )
     )
 
     println(
-        minScore(
-            4,
+        minReorder(
+            5,
             arrayOf(
-                intArrayOf(1,2,2),
-                intArrayOf(1,3,4),
-                intArrayOf(3,4,7),
+                intArrayOf(1,0),
+                intArrayOf(1,2),
+                intArrayOf(3,2),
+                intArrayOf(3,4),
+            )
+        )
+    )
+
+    println(
+        minReorder(
+            3,
+            arrayOf(
+                intArrayOf(1,0),
+                intArrayOf(2,0),
             )
         )
     )
 }
 
-fun minScore(n: Int, roads: Array<IntArray>): Int {
-    val isVisited = hashSetOf<Int>()
+fun minReorder(n: Int, connections: Array<IntArray>): Int {
+    val visited = IntArray(n)
+    val ways = Array(n) { LinkedList<Int>() }
 
-    roads.forEach {
+    var change = 0
 
+    connections.forEach {
+        ways[it[0]].add(it[1])
     }
 
+    ways.indices.forEach { index ->
+        if (visited[index] != 1){
+            change += dfs(visited, ways, index)
+        }
+    }
+
+    return change
+}
+
+private fun dfs(
+    visited: IntArray,
+    ways: Array<LinkedList<Int>>,
+    currentCity: Int
+): Int {
+    var countChange = 0
+
+    visited[currentCity] = 1
+    ways[currentCity].forEach { nextCity ->
+        if (visited[nextCity] == 0){
+            countChange += dfs(visited, ways, nextCity) + 1
+        }
+    }
+
+    return countChange
 }
