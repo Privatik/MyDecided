ce414f70078112c765b3d0ddc86cc40fe1f95c77
Android dev <i.kurak@complitech.org>
2023-03-29 10:05:53 +0300

    1519. Number of Nodes in the Sub-Tree With the Same Label

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
 import java.util.*
+import kotlin.collections.HashMap
 
 fun main(args: Array<String>) {
     println(
@@ -13,7 +14,7 @@ fun main(args: Array<String>) {
                 intArrayOf(2,6),
             ),
             "abaedcd"
-        )
+        ).joinToString(" ")
     )
 
     println(
@@ -25,7 +26,7 @@ fun main(args: Array<String>) {
                 intArrayOf(0,3),
             ),
             "bbbb"
-        )
+        ).joinToString(" ")
     )
 
 
@@ -39,10 +40,58 @@ fun main(args: Array<String>) {
                 intArrayOf(0,4),
             ),
             "aabab"
-        )
+        ).joinToString(" ")
+    )
+
+    println(
+        countSubTrees(
+            4,
+            arrayOf(
+                intArrayOf(0,2),
+                intArrayOf(0,3),
+                intArrayOf(1,2),
+            ),
+            "aabab"
+        ).joinToString(" ")
     )
 }
 
 fun countSubTrees(n: Int, edges: Array<IntArray>, labels: String): IntArray {
+    val result = IntArray(n) { 0 }
+    val vectors = Array<LinkedList<Int>>(n) { LinkedList() }
 
+    edges.forEach {
+        vectors[it[0]].add(it[1])
+        vectors[it[1]].add(it[0])
+    }
+
+    dfs(result, labels, vectors, 0, -1)
+
+    return result
+}
+
+fun dfs(
+    result: IntArray,
+    label: String,
+    vectors: Array<LinkedList<Int>>,
+    currentIndex: Int,
+    prevIndex: Int,
+): HashMap<Char, Int> {
+    val currentChar = label[currentIndex]
+    val cache: HashMap<Char, Int> = hashMapOf()
+
+    vectors[currentIndex].forEach { nextIndex ->
+        if (nextIndex != prevIndex){
+            val timeCache = dfs(result, label, vectors, nextIndex, currentIndex)
+
+            timeCache.forEach { (c, i) ->
+                cache[c] = cache.getOrDefault(c, 0) + i
+            }
+        }
+    }
+
+    cache[currentChar] = cache.getOrDefault(currentChar, 0) + 1
+    result[currentIndex] = cache[currentChar]!!
+
+    return cache
 }
