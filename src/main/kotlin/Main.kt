b7f1c057224f715f0ff24e0929b201e73a468bb3
Android dev <i.kurak@complitech.org>
2023-04-03 09:56:28 +0300

    Solved 33. Search in Rotated Sorted Array

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,10 @@
 fun main(args: Array<String>) {
-    println(
-        search(
-            intArrayOf(8,1,2,3,4,5,6,7),
-            6
-        )
-    )
+//    println(
+//        search(
+//            intArrayOf(8,1,2,3,4,5,6,7),
+//            6
+//        )
+//    )
 
     println(
         search(
@@ -65,38 +65,26 @@ fun main(args: Array<String>) {
 }
 
 fun search(nums: IntArray, target: Int): Int {
-    val isFoundInRight = nums.last() < nums.first() && nums.last() > target
-
-    var lower = 0
-    var upper = nums.lastIndex
-
-    while (lower < upper){
-        when (target) {
-            nums[lower] -> { return lower }
-            nums[upper] -> { return upper }
+    var low = 0
+    var high = nums.lastIndex
+    while (low <= high) {
+        val mid = (low + high) / 2
+        if (nums[mid] == target) {
+            return mid
         }
-
-        val mid = lower/2 + upper/2
-
-        when {
-            nums[mid] == target -> { return mid }
-            nums[mid] > target -> {
-                if (isFoundInRight){
-                    lower = mid + 1
-                } else {
-                    upper = mid - 1
-                }
+        if (nums[mid] >= nums[low]) {
+            if (target in nums[low]..nums[mid]) {
+                high = mid - 1
+            } else {
+                low = mid + 1
             }
-            nums[mid] < target -> {
-                if (isFoundInRight) {
-                    upper = mid - 1
-                } else {
-                    lower = mid + 1
-                }
+        } else {
+            if (target in nums[mid]..nums[high]) {
+                low = mid + 1
+            } else {
+                high = mid - 1
             }
         }
     }
-
-
-    return if (nums[lower] == target) lower else -1
+    return -1
 }
--- src/main/kotlin/OutSide.java
+++ src/main/kotlin/OutSide.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+
 class OutSide{
+
+    void doTest(){
+    }
 
     static class Inside {
         private static int count = 0;
@@ -9,6 +14,36 @@ class OutSide{
 
         int get(){
             return count;
+        }
+    }
+
+    void testing() throws Simple1 {
+        System.out.println("Hello world");
+    }
+
+    void testing1() throws Simple {
+        System.out.println("Hello world");
+    }
+
+    void testing2() throws Simple2 {
+        System.out.println("Hello world");
+    }
+
+    class Simple2 extends Throwable{
+        public Simple2(String message) {
+            super(message);
+        }
+    }
+
+    class Simple1 extends RuntimeException{
+        public Simple1(String message) {
+            super(message);
+        }
+    }
+
+    class Simple extends IOException{
+        public Simple(String message) {
+            super(message);
         }
     }
 }
