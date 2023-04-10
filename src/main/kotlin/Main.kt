9b24668e7f4ce79bbf78e5b1847fd7cb0534c91c
Android dev <i.kurak@complitech.org>
2023-04-10 08:40:20 +0300

    Solved 162. Find Peak Element

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,6 @@
 fun main(args: Array<String>) {
     println(
-        findPeakElement(intArrayOf(1,0,1,3,3,5,6,4))
+        findPeakElement(intArrayOf(1,2,1,3,5,6,4))
     )
 
     println(
@@ -10,22 +10,25 @@ fun main(args: Array<String>) {
 }
 
 fun findPeakElement(nums: IntArray): Int {
-    if (nums.size == 1) return 0
+    if(nums.size == 1) return 0
 
-    val n: Int = nums.size
+    val n = nums.size
 
-    if (nums[0] > nums[1]) return 0
-    if (nums[n - 1] > nums[n - 2]) return n - 1
+    // check if 0th/n-1th index is the peak element
+    if(nums[0] > nums[1]) return 0
+    if(nums[n-1] > nums[n-2]) return n-1
 
+    // search in the remaining array
     var start = 1
-    var end = n - 2
+    var end = n-2
 
-    while (start <= end) {
-        val mid = start + (end - start) / 2
-        if (nums[mid] > nums[mid - 1] && nums[mid] > nums[mid + 1]) return mid else if (nums[mid] < nums[mid - 1]) end =
-            mid - 1 else if (nums[mid] < nums[mid + 1]) start = mid + 1
+    while(start <= end) {
+        val mid = start + (end - start)/2;
+        if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid
+        else if(nums[mid] < nums[mid-1]) end = mid - 1
+        else if(nums[mid] < nums[mid+1]) start = mid + 1
     }
-    return -1
+    return -1; // dummy return statement
 }
 
 
