799d511a4ff6a024a1582ecf9b33fa5ed2f84eb1
Android dev <i.kurak@complitech.org>
2023-05-17 08:23:44 +0300

    1004. Max Consecutive Ones III

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,34 +3,62 @@ import java.util.*
 fun main(args: Array<String>) {
 
     println(
-        maxVowels("abciiidef",3)
+        longestOnes(intArrayOf(1,1,1,0,0,0,1,1,1,1,0),2)
     )
 
     println(
-        maxVowels("aeiou",2)
-    )
-
-    println(
-        maxVowels("leetcode",3)
+        longestOnes(intArrayOf(0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1),3)
     )
 
 }
 
-private val vowel = intArrayOf(1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0)
 
+fun longestOnes(nums: IntArray, k: Int): Int {
+    var max = 0
+    var countCanUse = k
+    var index = 0
+    var lastAvailableZeroIndex = 0
 
-fun maxVowels(s: String, k: Int): Int {
-    var r0 = 0
-    for (i in 0 until k)
-        r0 += vowel[s[i] - 'a']
-    var r = r0
-    for (i in k until s.length) {
-        r0 -= vowel[s[i - k] - 'a']
-        r0 += vowel[s[i] - 'a']
-        if (r0 > r)
-            r = r0
+    while (countCanUse > 0 && index < nums.size){
+        if (nums[index] == 0 && countCanUse > 0){
+            countCanUse--
+            lastAvailableZeroIndex = index
+        }
+
+        index++
+        max++
     }
-    return r
+
+    var nextMax = max
+    while (index < nums.size){
+        if (nextMax > max){
+            max = nextMax
+        }
+
+        if (nums[index] == 1){
+            nextMax++
+            index++
+            continue
+        }
+        if (nums[index] == 0){
+            if (countCanUse > 0){
+                nextMax++
+                countCanUse--
+                index++
+                continue
+            }
+            if (nums[index - max] == 1){
+                nextMax = k + (index - lastAvailableZeroIndex)
+            }
+            index++
+        }
+    }
+
+    if (nextMax > max){
+        max = nextMax
+    }
+
+    return max
 }
 
 class ListNode(var `val`: Int) {
