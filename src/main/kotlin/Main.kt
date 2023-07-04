23bff4de110bc99b458bff2a2442c0ad679d7c6b
Android dev <i.kurak@complitech.org>
2023-07-04 08:24:33 +0300

    1493. Longest Subarray of 1's After Deleting One Element

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,70 +1,43 @@
 import java.util.*
+import kotlin.math.max
 
 fun main() {
 
 
-//    println(
-//        longestOnes(intArrayOf(1,1,1,0,0,0,1,1,1,1,0),2)
-//    )
+    println(
+        longestSubarray(intArrayOf(1,1,0,1))
+    )
 
     println(
-        longestOnes(intArrayOf(0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1),3)
+        longestSubarray(intArrayOf(0,1,1,1,0,1,1,0,1))
     )
+
+    println(
+        longestSubarray(intArrayOf(1,1,1))
+    )
+
 
 }
 
 
-fun longestOnes(nums: IntArray, k: Int): Int {
+fun longestSubarray(nums: IntArray): Int {
     var max = 0
-    var countCanUse = k
-    var index = 0
-    var lastAvailableZeroIndex = 0
+    var canDeleteZeros = 1
+    var start = 0
 
-    while (countCanUse > 0 && index < nums.size){
-        val current = nums[index]
-        if (current == 0 && countCanUse > 0){
-            countCanUse--
-            lastAvailableZeroIndex = index + 1
-        }
-
-        index++
-        max++
-    }
-
-    println("exit from fist cycle index = ${index} max = ${max} lastAvailableZeroIndex = ${lastAvailableZeroIndex}")
-
-    var nextMax = max
-    while (index < nums.size){
-        if (nextMax > max){
-            max = nextMax
-        }
-
-        if (nums[index] == 1){
-            nextMax++
-            index++
-            println("index = 1 , current index $index ${nextMax}")
-            continue
-        }
-        if (nums[index] == 0){
-            println("index = 0 , current index $index ${nextMax}")
-            if (countCanUse > 0){
-                nextMax++
-                countCanUse--
-                index++
-                lastAvailableZeroIndex = index
-                println("countCanUse was more 0 , current index $index ${nextMax}")
-                continue
+    var end = 0
+    while (end < nums.size){
+        if (nums[end] == 0){
+            if (canDeleteZeros == 1){
+                canDeleteZeros = 0
+            } else {
+                start++
+                if (nums[start] == 1) { end-- }
             }
-            if (nums[index - max] == 1){
-                nextMax = k + (index - lastAvailableZeroIndex)
-                println("update , current index $index ${nextMax}")
-            }
-            index++
         }
-    }
 
-    if (nextMax > max){
-        max = nextMax
+        max = max(end - start, max)
+        end++
     }
 
     return max
