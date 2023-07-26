0796d6c9e54e8dc18f8dd87177cf3ce6f13ee987
Privatik <kurak.van9@gmail.com>
2023-07-26 07:57:59 +0300

    394. Decode String

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -8,69 +8,69 @@ const val codeA = 1040
 fun main() {
 
     println(
-        asteroidCollision(intArrayOf(5,10,-5)).joinToString(" ")
+        decodeString("3[a]2[bc]")
     )
 
     println(
-        asteroidCollision(intArrayOf(8,-8)).joinToString(" ")
+        decodeString("3[a2[c]]")
     )
 
     println(
-        asteroidCollision(intArrayOf(10,2,-5)).joinToString(" ")
+        decodeString("2[abc]3[cd]ef")
     )
 
 }
 
-fun asteroidCollision(asteroids: IntArray): IntArray {
-    val stack = Stack<Int>()
-    var index = 1
+fun decodeString(s: String): String {
+    val builder = StringBuilder()
 
-    stack.push(asteroids[0])
-    while (index < asteroids.size && !stack.isEmpty()){
-        val valueBefore = stack.peek()
-        val dif = valueBefore + asteroids[index]
+    val stack = Stack<Char>()
+    val digits = Stack<Int>()
 
-        if (dif == 0){
-            stack.pop()
-            val nextValue = asteroids.getOrNull(index + 1)
-            if (nextValue != 0){ stack.push(nextValue) }
-            index += 2
-            continue
-        }
-
-        if (valueBefore > 0){
-            if (dif < 0) {
-                stack.pop();
-                stack.push(asteroids[index])
-            }
-            else if (dif > valueBefore){
-                stack.push(asteroids[index])
-            }
-        } else if (valueBefore < 0){
-            if (dif > 0) {
-                stack.pop();
-                stack.push(asteroids[index])
-            }
-            else if (dif < valueBefore){
-                stack.push(asteroids[index])
-            }
+    var digit = 0
+    var index = 0
+    while (index < s.length){
+        if (s[index].isDigit()) { digit = digit*10 + (s[index] - '0') }
+        else if (s[index] == '[') {
+            digits.push(digit)
+            digit = 0
+            if (s[index] == ']')
+        } else {
+            stack.push(s[index])
         }
         index++
     }
+}
 
-    if (stack.size == 1){
-        if (stack.peek() == null){
-            return intArrayOf()
+private fun decodeString(
+    builder: StringBuilder,
+    s: String,
+    index: Int,
+) : Int {
+    var digit = 0
+    var cIndex = index
+    while (cIndex < s.length){
+        if (s[cIndex].isDigit()) { digit = digit*10 + (s[cIndex] - '0') }
+        else if (s[cIndex] == '[') {
+            var insideIndex = cIndex + 1
+            while (digit != 0){
+                if (s[insideIndex] == '['){
+                    insideIndex = decodeString(builder, s, insideIndex)
+                } else {
+                    builder.append(s[insideIndex])
+                    insideIndex++
+                }
+                if (s[insideIndex] == ']'){
+                    if (--digit != 0) insideIndex = cIndex + 1
+                }
+            }
+            cIndex = insideIndex
+        } else {
+            builder.append(s[cIndex])
         }
+        cIndex++
     }
-
-    val result = IntArray(stack.size)
-
-    result.indices.reversed().forEach { insideIndex ->
-        result[insideIndex] = stack.pop()
-    }
-
-    return result
+    return cIndex
 }
 
 //fun ListNode.getValues(): StringBuilder{
