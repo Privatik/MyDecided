24c169b492b373215cf0f2aaef1f4bc6d10ae800
Android dev <i.kurak@complitech.org>
2023-07-28 16:19:31 +0300

    121. Best Time to Buy and Sell Stock

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -6,56 +6,83 @@ import kotlin.math.log10
 const val codeA = 1040
 
 fun main() {
+//    val counter = RecentCounter()
+//
+//    println(
+//        counter.ping(1)
+//    )
+//
+//    println(
+//        counter.ping(100)
+//    )
+//
+//    println(
+//        counter.ping(3001)
+//    )
+//
+//    println(
+//        counter.ping(3002)
+//    )
 
     println(
-        decodeString("3[a]2[bc]")
+        maxProfit(intArrayOf(7,1,5,3,6,4))
     )
 
     println(
-        decodeString("3[a2[c]]")
+        maxProfit(intArrayOf(7,6,4,3,1))
     )
 
     println(
-        decodeString("2[abc]3[cd]ef")
+        maxProfit(intArrayOf(1,2,3))
     )
 
+    println(
+        maxProfit(intArrayOf(6,1,3,2,4,7))
+    )
 }
 
-fun decodeString(s: String): String {
-    val builder = StringBuilder()
+class RecentCounter() {
+    private val queue: Queue<Int> = LinkedList()
 
-    decodeString(builder, s, 0)
-
-    return builder.toString()
-}
-
-private fun decodeString(
-    builder: StringBuilder,
-    s: String,
-    index: Int,
-) : Int {
-
-    var digit = 0
-    var cIndex = index
-    while (cIndex < s.length){
-        if (s[cIndex].isDigit()) { digit = digit*10 + (s[cIndex] - '0') }
-        else if (s[cIndex] == '[') {
-            var insideIndex = cIndex + 1
-            while (digit != 0){
-                insideIndex = decodeString(builder, s, insideIndex)
-                if (s[insideIndex] == ']'){
-                    if (--digit != 0) insideIndex = cIndex + 1
-                }
-            }
-            cIndex = insideIndex
-        } else if (s[cIndex] == ']') {
-            return cIndex
-        } else {
-            builder.append(s[cIndex])
-        }
-        cIndex++
+    fun ping(t: Int): Int {
+        queue.offer(t)
+        while (queue.peek() < t - 3000){ queue.poll() }
+        return queue.size
     }
-    return cIndex
+
+
+}
+
+fun maxProfit(prices: IntArray): Int {
+    val dp = Array(prices.size) { IntArray(prices.size) { Int.MIN_VALUE } }
+
+    dp[0][0] = 0
+
+    if (prices.size > 1){
+        if (prices[1] - prices[0] > 0){
+            dp[0][1] = -prices[0]
+        }
+    }
+
+    (1 until prices.size).forEach { firstIndex->
+        (0..firstIndex).forEach { secondIndex ->
+            val next = (dp[firstIndex - 1].getOrNull(secondIndex + 1) ?: Int.MIN_VALUE)
+
+            dp[firstIndex][secondIndex] = maxOf(
+                dp[firstIndex - 1][secondIndex],
+                -prices[firstIndex] ,
+                if (next <= 0) { next + prices[firstIndex] } else Int.MIN_VALUE
+            )
+        }
+    }
+
+    println("-------")
+    println(
+        dp.joinToString("\n") { it.joinToString(" ") }
+    )
+    println("-------")
+
+    return dp.last().first()
 }
 
 //fun ListNode.getValues(): StringBuilder{
