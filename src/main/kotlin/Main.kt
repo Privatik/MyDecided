45837faccec5a91fd77f9b1d360f7267b0861540
Privatik <kurak.van9@gmail.com>
2023-08-02 07:41:31 +0300

    2095. Delete the Middle Node of a Linked List

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -24,53 +24,74 @@ fun main() {
 //    )
 
     println(
-        predictPartyVictory("RD")
+        deleteMiddle(
+            createListNode(values = intArrayOf(1,3,4,7,1,2,6))
+        )?.getValues()
     )
 
     println(
-        predictPartyVictory("RDD")
+        deleteMiddle(
+            createListNode(values = intArrayOf(1,2,3,4))
+        )?.getValues()
+    )
+
+    println(
+        deleteMiddle(
+            createListNode(values = intArrayOf(2,1))
+        )?.getValues()
     )
 }
 
-//DA
-
-fun predictPartyVictory(senate: String): String {
-    val queue: Queue<Char> = LinkedList()
-
-    senate.forEach { item -> queue.offer(item) }
-    queue.offer('A')
-    var isOnlyOneParties = true
-
-    while (queue.size != 2){
-        val value = queue.poll()
-        var nextValue = queue.peek()
-        queue.offer(value)
-
-        if (nextValue == 'A') {
-            if (isOnlyOneParties) { break }
-            queue.poll()
-            nextValue = queue.peek()
-            queue.offer('A')
-        }
-
-        if (value != nextValue){
-            isOnlyOneParties = false
-            queue.poll()
-        }
-    }
-
-    return if (queue.peek() == 'R') "Radiant" else "Dire"
+fun deleteMiddle(head: ListNode?): ListNode? {
+    val preMiddleNode = findPreMiddle(0,0, head, null)
+    preMiddleNode?.next = preMiddleNode?.next?.next
+    return if (preMiddleNode == null) head?.next else head
 }
 
-//fun ListNode.getValues(): StringBuilder{
-//    val builder = StringBuilder()
-//    builder.append(`val`)
-//    if (next != null){
-//        builder.append(" ")
-//        builder.append(next!!.getValues())
-//    }
-//    return builder
-//}
+private fun findPreMiddle(
+    middleIndex: Int,
+    currentSize: Int,
+    head: ListNode?,
+    currentPreMiddle: ListNode?,
+): ListNode? {
+    var nextPreMiddle: ListNode? = currentPreMiddle
+    var _middleIndex = middleIndex
+
+    if ((currentSize + 1) / 2 != middleIndex){
+        nextPreMiddle = if (currentPreMiddle == null) head else currentPreMiddle.next
+        _middleIndex++
+    }
+
+    if (head?.next != null){
+        nextPreMiddle = findPreMiddle(_middleIndex,currentSize + 1, head.next, nextPreMiddle)
+    }
+
+    return nextPreMiddle
+}
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun createListNode(
+    startIndex: Int = 0,
+    values: IntArray,
+): ListNode? {
+    if (startIndex !in values.indices) return null
+    val node = ListNode(values[startIndex])
+    node.next = createListNode(startIndex + 1, values)
+    return node
+}
+
+fun ListNode.getValues(): StringBuilder{
+    val builder = StringBuilder()
+    builder.append(`val`)
+    if (next != null){
+        builder.append(" ")
+        builder.append(next!!.getValues())
+    }
+    return builder
+}
 
 
 
