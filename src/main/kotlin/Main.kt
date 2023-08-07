554a4457b3702c9346bf2377ec4502e1b711abec
Android dev <i.kurak@complitech.org>
2023-08-07 10:43:48 +0300

    Solved 268. Missing Number

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,15 +4,63 @@ import kotlin.math.min
 const val codeA = 1040
 
 fun main() {
+//
+//    println(
+//        oddEvenList(createListNode(array = intArrayOf(1,2,3,4,5)))?.getValues()
+//    )
 
     println(
-        oddEvenList(createListNode(array = intArrayOf(1,2,3,4,5)))?.getValues()
+        missingNumber(intArrayOf(1))
     )
 
     println(
-        oddEvenList(createListNode(array = intArrayOf(2,1,3,5,6,4,7)))?.getValues()
+        missingNumber(intArrayOf(3,0,1))
     )
 
+    println(
+        missingNumber(intArrayOf(0,1))
+    )
+
+    println(
+        missingNumber(intArrayOf(9,6,4,2,3,5,7,0,1))
+    )
+
+    println(
+        missingNumber(intArrayOf(0,3,5,8,4,6,1,9,7))
+    )
+
+}
+
+fun missingNumber(nums: IntArray): Int {
+    nums.sort()
+
+    var left = 0
+    var right = nums.size - 1
+    while (left <= right){
+        if (right - left < 3){
+            (left..right).forEach { index ->
+                if (index != nums[index]) return index
+            }
+            break
+        }
+
+        val mid = ((left + right) / 2) + 1
+        val item = nums.binarySearch(mid, left, right)
+        if (item < 0) return mid
+
+        if (item < mid){
+            right = mid
+        } else {
+            left = mid + 1
+        }
+    }
+
+
+    return nums.size
+}
+
+fun findDelayedArrivalTime(arrivalTime: Int, delayedTime: Int): Int {
+    return (arrivalTime + delayedTime) % 24
 }
 
 
