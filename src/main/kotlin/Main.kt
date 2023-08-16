3258bfad2405db09612da4472db1e6dcb2dcf4d9
Privatik <kurak.van9@gmail.com>
2023-08-16 08:10:51 +0300

    872. Leaf-Similar Trees

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,5 @@
-import kotlin.math.pow
+import java.util.LinkedList
+import java.util.Queue
 
 const val codeA = 1040
 
@@ -29,24 +30,17 @@ fun main() {
 //    )
 
     println(
-        sumRootToLeaf(
-            TreeNode(1).apply {
-                left = TreeNode(0).apply {
-                    left = TreeNode(0)
-                    right = TreeNode(1)
-                }
-                right = TreeNode(1).apply {
-                    left = TreeNode(0)
-                    right = TreeNode(1)
-                }
-            }
-        )
+        createTreeNode(0, listOf(3,5,1,6,2,9,8,null,null,7,4))?.getValues()
     )
 
     println(
-        sumRootToLeaf(TreeNode(0))
+        createTreeNode(0, listOf(3,5,1,6,7,4,2,null,null,null,null,null,null,9,8))?.getValues()
     )
 
+}
+
+fun leafSimilar(root1: TreeNode?, root2: TreeNode?): Boolean {
+    return false
 }
 
 fun sumRootToLeaf(root: TreeNode?, current: Int = 0): Int {
@@ -61,54 +55,6 @@ fun sumRootToLeaf(root: TreeNode?, current: Int = 0): Int {
 //    return dfs(space, root, 0)
 //}
 
-fun dfs(
-    space: IntArray,
-    node: TreeNode?,
-    currentIndex: Int,
-): Int {
-    var sum = 0
-    if (node == null) return sum
-
-    space[currentIndex] = node.`val`
-    if (node.left == null && node.right == null){
-        (0..currentIndex).forEach { index ->
-            sum += 2.0.pow(index.toDouble()).toInt() * space[currentIndex - index]
-        }
-    } else {
-        sum += dfs(space, node.left, currentIndex + 1)
-        sum += dfs(space, node.right, currentIndex + 1)
-    }
-
-    return sum
-}
-
-fun missingNumber(nums: IntArray): Int {
-    nums.sort()
-
-    var left = 0
-    var right = nums.size - 1
-    while (left <= right){
-        if (right - left < 3){
-            (left..right).forEach { index ->
-                if (index != nums[index]) return index
-            }
-            break
-        }
-
-        val mid = ((left + right) / 2) + 1
-        val item = nums.binarySearch(mid, left, right)
-        if (item < 0) return mid
-
-        if (item < mid){
-            right = mid
-        } else {
-            left = mid + 1
-        }
-    }
-
-
-    return nums.size
-}
 
 fun findDelayedArrivalTime(arrivalTime: Int, delayedTime: Int): Int {
     return (arrivalTime + delayedTime) % 24
@@ -153,6 +99,39 @@ fun createListNode(index: Int = 0, array: IntArray): ListNode? {
     val node = ListNode(array[index])
     node.next = createListNode(index + 1, array)
     return node
+}
+
+fun createTreeNode(index: Int = 0, list: List<Int?>): TreeNode? {
+    if (index !in list.indices) return null
+    if (list[index] == null) return null
+    val node = TreeNode(list[index]!!)
+    node.left = createTreeNode((index*2) + 1, list)
+    node.right = createTreeNode((index*2) + 2, list)
+    return node
+}
+
+fun TreeNode.getValues(): String{
+    return getValues(this).joinToString(" ")
+}
+
+fun TreeNode.getValues(
+    node: TreeNode,
+) : List<Int?> {
+    val answer = mutableListOf<Int?>()
+    val queue: Queue<TreeNode> = LinkedList()
+    answer.add(`val`)
+    queue.offer(node)
+
+    while (queue.isNotEmpty()){
+        val currentNode = queue.poll()
+        answer.add(currentNode.left?.`val`)
+        answer.add(currentNode.right?.`val`)
+
+        if (currentNode.left != null) queue.offer(currentNode.left)
+        if (currentNode.right != null) queue.offer(currentNode.right)
+    }
+
+    return answer
 }
 
 class TreeNode(var `val`: Int) {
